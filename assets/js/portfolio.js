fcaa8ec4141e5d83324db9f92cb969be95b59024
Katerina <yelkaterina@gmail.com>
2023-10-02 15:43:53 +0200

    Update projects section

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,5 +1,4 @@
-// portfolios
-
+// projects
 let projects = [
     {
         title: "Running pace calculator",
@@ -14,34 +13,57 @@ let projects = [
         repo: "https://github.com/e-kai00/expense-tracker",
         website: "https://kai00-expense-tracker.herokuapp.com/accounts/login/?next=/",
         tools: ["HTML", "CSS", "jQuery", "Python", "Django", "ElephantSQL"]
+    },
+    {
+        title: " Expense Tracker",
+        image: "test",
+        repo: "https://github.com/e-kai00/expense-tracker",
+        website: "https://kai00-expense-tracker.herokuapp.com/accounts/login/?next=/",
+        tools: ["HTML", "CSS", "jQuery", "Python", "Django", "ElephantSQL"]
+    },
+    {
+        title: " Expense Tracker",
+        image: "test",
+        repo: "https://github.com/e-kai00/expense-tracker",
+        website: "https://kai00-expense-tracker.herokuapp.com/accounts/login/?next=/",
+        tools: ["HTML", "CSS", "jQuery", "Python", "Django", "ElephantSQL"]
     }
 ];
 
+// Shields.io badges
+const badges = {
+    html: 'https://img.shields.io/badge/html-%23ffffff?logo=html5&logoColor=%23E34F26',
+    css: 'https://img.shields.io/badge/css-%23ffffff?logo=css3&logoColor=%23E34F26',
+    javascript:'https://img.shields.io/badge/JavaScript-%23ffffff?logo=javascript&logoColor=%23E34F26'
+}
+
 
 // map each project to own <li> item
-
 let li_project = "";
 
 projects.map((project) => {
-    li_project += `<li><h3>${project.title}</h3><img src="assets/img/${project.image}.png" class="portfolio-img">`;
-    if (project.repo) {
-        li_project += `<a href="${project.repo}" target="_blank">Repo</a>`;
-    }
-    if (project.website) {
-        li_project += `<a href="${project.website}" target="_blank">Website</a>`;
-    }
-    
-    li_project += `<ul>`
+    li_project += `<li><figure><h3 class="project-title">${project.title}</h3><img src="assets/img/${project.image}.png" class="portfolio-img"><figcaption><ul class="container-badges center-align">`;
+        
     if (project.tools) {
         project.tools.forEach((tool) => {
-            li_project += `<li>${tool}</li>`
+            const badgeUrl = badges[tool.toLowerCase()]
+            if (badgeUrl) {
+                li_project += `<li><img src="${badgeUrl}"</li>`
+            }
         })
     }
-    li_project += `</ul></li>`
+    li_project += `</ul>`
+    if (project.repo) {
+        li_project += `<a href="${project.repo}" class="btn-small right" rel="noopener" target="_blank">Repo <i class="fa-brands fa-github"></i></a>`;
+    }
+    if (project.website) {
+        li_project += `<a href="${project.website}" class="btn-small right" rel="noopener" target="_blank">Link <i class="fas fa-link"></i></a>`;
+    }
+    li_project += `</figcaption></figure></li>`
 });
 
 // add the <li> items to the main <ul>
-let ul_project = `<ul>${li_project}</ul>`
+let ul_project = `<ul class="project-list">${li_project}</ul>`
 
 // append <ul> to portfolio section
 let portfolio_section = document.getElementById("portfolio");
